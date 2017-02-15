375a475bb31c48db498dda3b3cb323add78eaa00
John Watzke <watzkej@gmail.com>
2017-02-14 20:54:50 -0600

    trying cleardb

--- wp-config.php
+++ wp-config.php
@@ -19,7 +19,7 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-$url = parse_url(getenv('DATABASE_URL'));
+$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
 
 /** The name of the database for WordPress */
 define('DB_NAME', trim($url['path'], '/'));
